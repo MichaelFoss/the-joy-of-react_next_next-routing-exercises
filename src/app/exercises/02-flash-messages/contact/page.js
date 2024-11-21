90f9242f869b8524f545a086ddf54b8c4e332237
Michael Foss <michael.foss@mdpanel.com>
2024-11-21 07:48:53 -0500

    Complete exercise 2

--- src/app/exercises/02-flash-messages/contact/page.js
+++ src/app/exercises/02-flash-messages/contact/page.js
@@ -1,10 +1,21 @@
 'use client';
-import React from 'react';
+import React, { useContext } from 'react';
+import { ToastContext } from '../../../../components/ToastProvider';
+import { useRouter } from 'next/navigation';
 
 function ContactPage() {
+  const router = useRouter();
+  const { createToast } = useContext(ToastContext);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    createToast('Got your message!', 'notice');
+    router.push('/exercises/02-flash-messages');
+  };
+
   return (
     <main>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="name">Name:</label>
         <input id="name" required={true} />
 
